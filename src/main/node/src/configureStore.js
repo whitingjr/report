61eeda335ca6b2268f02dd40d17072b960f1df65
Will Reichert <willr3@gmail.com>
2019-06-19 11:49:07 -0500

    use hashHistory to support file:// url

--- src/main/node/src/configureStore.js
+++ src/main/node/src/configureStore.js
@@ -3,8 +3,7 @@ import {applyMiddleware, createStore} from 'redux'
 import {routerMiddleware} from 'connected-react-router'
 import createRootReducer from './reducers'
 
-
-export const history = createBrowserHistory();//createHashHistory();
+export const history = createHashHistory();//createBrowserHistory();//
 export default function configureStore(preloadedState){
     const store = createStore(
         createRootReducer(history),
